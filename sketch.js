46a222b345c30819804741128778ddfe71afb78e
Sponky745 <sponkyblox@gmail.com>
2021-12-19 11:56:40 +0000

    lol

--- sketch.js
+++ sketch.js
@@ -1,13 +1,17 @@
+let pos;
+
 function fullScreen() {
 	createCanvas(windowWidth, windowHeight);
 }
 
 function setup() {
 	fullScreen();
+	pos = createVector(width/2, height/2);
 }
 
 function draw() {
-	background(255);
+	background(0);
 	fill(0, 255, 255);
-	circle(width/2, height/2, 64);
+	circle(pos.x, pos.y, 64);
+	pos.add(p5.Vector.fromAngle(atan2(mouseY-pos.y, mouseX-pos.x)));
 }
