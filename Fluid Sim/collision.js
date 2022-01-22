10a1b6137d00329b6ee7c639642a13c38e6c9e11
Sponky745 <65668416+Sponky745@users.noreply.github.com>
2022-01-22 11:34:50 +0000

    Update collision.js

--- Fluid Sim/collision.js
+++ Fluid Sim/collision.js
@@ -1,3 +1,18 @@
+function collResBB(b1, b2) {
+  let normal = p5.Vector.sub(b1.pos, b2.pos).normalize();
+  let relVel = p5.Vector.sub(b1.vel, b2.vel);
+  let sepVel = p5.Vector.dot(relVel, normal);
+  let new_sepVel = -sepVel;
+  let sepVelVec = p5.Vector.mult(normal, new_sepVel);
+
+  let vsepDiff = new_sepVel - sepVel;
+  let impulse = vsepDiff;
+  let impulseVec = p5.Vector.mult(normal, impulse);
+
+  b1.vel.add(impulseVec);
+  b2.vel.add(impulseVec.mult(-1));
+}
+
 function penResBB(b1, b2) {
   let dist = p5.Vector.sub(b1.pos, b2.pos);
   let depth = b1.r + b2.r - dist.mag();
